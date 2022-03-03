6b5fdf8df91d13e2e978b04c916a566028d4f7db
Chanukya <97583921+Chanukya113118@users.noreply.github.com>
2022-03-03 19:57:32 +0530

    Add files via upload

---
+++ question_based.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import viz_opt 
+st.title("Question the Data -- Question's Based")
+file=st.file_uploader('Upload file',type=['CSV'])
+if file is not None:
+    data=pd.read_csv(file)
+    st.title('What do you want to do with your data?')
+    opt=st.radio('choose',options=['','Visualization','Analyses','Prediction'])
+    if opt=='Visualization':
+        viz_opt.viz(data)
+    elif opt=='Analyses':
+        st.write('Wait coming')
+    elif opt=='Prediction':
+        st.write('wait Coming')
