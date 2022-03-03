fbba7b0eeb6d8be3ce8276d0e629374ed0fe48e0
Chanukya <97583921+Chanukya113118@users.noreply.github.com>
2022-03-03 20:01:52 +0530

    Update question_based.py

--- question_based.py
+++ question_based.py
@@ -3,13 +3,16 @@ import pandas as pd
 import viz_opt 
 st.title("Question the Data -- Question's Based")
 file=st.file_uploader('Upload file',type=['CSV'])
-if file is not None:
-    data=pd.read_csv(file)
-    st.title('What do you want to do with your data?')
-    opt=st.radio('choose',options=['','Visualization','Analyses','Prediction'])
-    if opt=='Visualization':
-        viz_opt.viz(data)
-    elif opt=='Analyses':
-        st.write('Wait coming')
-    elif opt=='Prediction':
-        st.write('wait Coming')
+try:
+    if file is not None:
+        data=pd.read_csv(file)
+        st.title('What do you want to do with your data?')
+        opt=st.radio('choose',options=['','Visualization','Analyses','Prediction'])
+        if opt=='Visualization':
+            viz_opt.viz(data)
+        elif opt=='Analyses':
+            st.write('Wait coming')
+        elif opt=='Prediction':
+            st.write('wait Coming')
+except:
+    st.write('Try only CSV files')
